cdd2905edde9f70f2a4f1e7d00023ce0c7fd0bb1
Wynter Woods <zerotri@gmail.com>
2021-09-09 02:25:16 -0700

    Add more unit tests for MutableArray, currently segfaulting from a container bug

--- tests/test_containers.cpp
+++ tests/test_containers.cpp
@@ -15,4 +15,34 @@ TEST_CASE("MutableArray") {
         REQUIRE(testArray.AllocatedSize() >= 10);
         REQUIRE(testArray[0] == "This is a test string");
     }
+
+    SUBCASE("Removing from the array decreases Count") {
+        testArray.Remove(0);
+
+        REQUIRE(testArray.Count() == 0);
+        REQUIRE(testArray.AllocatedSize() >= 10);
+    }
+
+    SUBCASE("Adding many elements to the array increases AllocatedSize") {
+        REQUIRE(testArray.AllocatedSize() >= 10);
+        REQUIRE(testArray.AllocatedSize() < 14);
+
+        REQUIRE(testArray.Add("This") == 1);
+        REQUIRE(testArray.Add("is") == 2);
+        REQUIRE(testArray.Add("a") == 3);
+        REQUIRE(testArray.Add("test") == 4);
+        REQUIRE(testArray.Add("attempting") == 5);
+        REQUIRE(testArray.Add("to") == 6);
+        REQUIRE(testArray.Add("increase") == 7);
+        REQUIRE(testArray.Add("AllocatedSize") == 8);
+        REQUIRE(testArray.Add("of") == 9);
+        REQUIRE(testArray.Add("MutableArray") == 10);
+        REQUIRE(testArray.Add("by") == 11);
+        REQUIRE(testArray.Add("adding") == 12);
+        REQUIRE(testArray.Add("14") == 13);
+        REQUIRE(testArray.Add("elements") == 14);
+
+        REQUIRE(testArray.Count() == 14);
+        REQUIRE(testArray.AllocatedSize() >= 14);
+    }
 }
